b18d59c13fbc5109ed91914cdc4ce6ab0986cdae
Ashish Sharda <ashishjsharda@gmail.com>
2023-04-17 08:26:13 -0500

    add runner and queue name

--- src/main/java/com/example/springamqpdemo/SpringamqpdemoApplication.java
+++ src/main/java/com/example/springamqpdemo/SpringamqpdemoApplication.java
@@ -2,8 +2,11 @@ package com.example.springamqpdemo;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListeners;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringamqpdemoApplication {
@@ -15,6 +18,20 @@ public class SpringamqpdemoApplication {
     @RabbitListeners(value = @RabbitListener(queues = "myqueue"))
     public void listen(String in) {
         System.out.println(in);
+    }
+
+    @Bean
+    public String queueName() {
+        return "myqueue";
+    }
+
+    @Bean
+    public ApplicationRunner runner(RabbitTemplate template, String queueName) {
+        return args -> {
+            template.convertAndSend(queueName, "Hello, world!");
+        };
+    }
+
     }
 
 }
