5997e03001671a11081e66e666c3c19ba1ec027b
Ashish Sharda <ashishjsharda@gmail.com>
2023-04-17 08:21:05 -0500

    add listener

--- src/main/java/com/example/springamqpdemo/SpringamqpdemoApplication.java
+++ src/main/java/com/example/springamqpdemo/SpringamqpdemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.springamqpdemo;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class SpringamqpdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringamqpdemoApplication.class, args);
+    }
+
+    @RabbitListeners(value = @RabbitListener(queues = "myqueue"))
+    public void listen(String in) {
+        System.out.println(in);
     }
 
 }
