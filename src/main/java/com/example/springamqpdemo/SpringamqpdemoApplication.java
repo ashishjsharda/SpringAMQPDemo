42e7b1b2c91c3737831b3113f901381cb5aac41e
Ashish Sharda <ashishjsharda@gmail.com>
2023-04-17 08:18:55 -0500

    first commit

---
+++ src/main/java/com/example/springamqpdemo/SpringamqpdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springamqpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringamqpdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringamqpdemoApplication.class, args);
+    }
+
+}
