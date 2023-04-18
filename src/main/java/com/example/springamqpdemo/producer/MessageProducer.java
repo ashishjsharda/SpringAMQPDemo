f36ded847da38c2695f8f6a3a39c1caeda3d934d
Ashish Sharda <ashishjsharda@gmail.com>
2023-04-18 06:15:17 -0500

    send message and constructor

--- src/main/java/com/example/springamqpdemo/producer/MessageProducer.java
+++ src/main/java/com/example/springamqpdemo/producer/MessageProducer.java
@@ -1,4 +1,18 @@
 package com.example.springamqpdemo.producer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageProducer(final RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend("myqueue", message);
+    }
+
 }
